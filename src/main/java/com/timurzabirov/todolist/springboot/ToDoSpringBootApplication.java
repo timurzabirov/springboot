5487abd98421227cdb0924c08e6b76747893e5de
Timur <tzabirov93@gmail.com>
2022-01-14 18:30:49 +0300

    Initial commit

---
+++ src/main/java/com/timurzabirov/todolist/springboot/ToDoSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.timurzabirov.todolist.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToDoSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/timurzabirov/todolist/springboot/ToDoSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timurzabirov.todolist.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
