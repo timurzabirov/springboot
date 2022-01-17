1d4ce54de08abd90112a53ddcef980931e926cc9
Timur <tzabirov93@gmail.com>
2022-01-17 21:08:15 +0300

    Priority and Category Controllers were modified.

--- src/main/java/com/timurzabirov/todolist/springboot/controller/CategoryController.java
+++ src/main/java/com/timurzabirov/todolist/springboot/controller/CategoryController.java
@@ -1,10 +1,13 @@
 package com.timurzabirov.todolist.springboot.controller;
 
+import com.timurzabirov.todolist.springboot.entity.Category;
 import com.timurzabirov.todolist.springboot.repository.CategoryRepository;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
 /*@RestController вместо @Controller для того, чтобы ответы были сразу в виде JSON.
-* Иначе надо было бы использовать @ResponseBody для ответа*/
+ * Иначе надо было бы использовать @ResponseBody для ответа*/
 @RestController
 @RequestMapping("/category") // Базовый адрес для всех методов класса
 public class CategoryController {
@@ -16,5 +19,39 @@ public class CategoryController {
      * Не используем @Autowired для переменной,т.к. Field injection is not recommended*/
     public CategoryController(CategoryRepository categoryRepository) {
         this.categoryRepository = categoryRepository;
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<Category> add(@RequestBody Category category) {
+
+        // Проверка на обязательные параметры
+        if (category.getId() != null && category.getId() != 0) {
+            return new ResponseEntity("Redundant param: id must be null", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        if (category.getTitle() == null && category.getTitle().trim().length() == 0) {
+            return new ResponseEntity("Missed param: title", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        // ResponseEntity - это объект-коробка, в который мы можем поместить объект и статус запроса
+        // Метод save() используется как для добавления, так и для обновления сущности
+        return ResponseEntity.ok(categoryRepository.save(category));
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<Category> update(@RequestBody Category category) {
+
+        // Проверка на обязательные параметры
+        if (category.getId() == null && category.getId() == 0) {
+            return new ResponseEntity("Redundant param: id must be null", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        if (category.getTitle() == null && category.getTitle().trim().length() == 0) {
+            return new ResponseEntity("Missed param: title", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        // ResponseEntity - это объект-коробка, в который мы можем поместить объект и статус запроса
+        // Метод save() используется как для добавления, так и для обновления сущности
+        return ResponseEntity.ok(categoryRepository.save(category));
     }
 }
--- src/main/java/com/timurzabirov/todolist/springboot/controller/PriorityController.java
+++ src/main/java/com/timurzabirov/todolist/springboot/controller/PriorityController.java
@@ -1,8 +1,11 @@
 package com.timurzabirov.todolist.springboot.controller;
 
+import com.timurzabirov.todolist.springboot.entity.Priority;
 import com.timurzabirov.todolist.springboot.repository.PriorityRepository;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
 /*@RestController вместо @Controller для того, чтобы ответы были сразу в виде JSON.
  * Иначе надо было бы использовать @ResponseBody для ответа*/
 @RestController
@@ -16,6 +19,46 @@ public class PriorityController {
      * Не используем @Autowired для переменной,т.к. Field injection is not recommended*/
     public PriorityController(PriorityRepository priorityRepository) {
         this.priorityRepository = priorityRepository;
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<Priority> add(@RequestBody Priority priority) {
+        // Проверка на обязательные параметры
+        if (priority.getId() != null && priority.getId() != 0) {
+            return new ResponseEntity("Redundant param: id must be null", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        if (priority.getTitle() == null && priority.getTitle().trim().length() == 0) {
+            return new ResponseEntity("Missed param: title", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        if (priority.getColor() == null && priority.getColor().trim().length() == 0) {
+            return new ResponseEntity("Missed param: title", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        // ResponseEntity - это объект-коробка, в который мы можем поместить объект и статус запроса
+        // Метод save() используется как для добавления, так и для обновления сущности
+        return ResponseEntity.ok(priorityRepository.save(priority));
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<Priority> update(@RequestBody Priority priority) {
+        // Проверка на обязательные параметры
+        if (priority.getId() == null && priority.getId() == 0) {
+            return new ResponseEntity("Redundant param: id must be null", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        if (priority.getTitle() == null && priority.getTitle().trim().length() == 0) {
+            return new ResponseEntity("Missed param: title", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        if (priority.getColor() == null && priority.getColor().trim().length() == 0) {
+            return new ResponseEntity("Missed param: title", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        // ResponseEntity - это объект-коробка, в который мы можем поместить объект и статус запроса
+        // Метод save() используется как для добавления, так и для обновления сущности
+        return ResponseEntity.ok(priorityRepository.save(priority));
     }
 
 }
